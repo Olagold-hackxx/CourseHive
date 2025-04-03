ad8fe563c1dc248425722907474ada71fc5886ac
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2025-04-03 11:29:22 +0100

    Setup springboot

---
+++ Backend/src/main/java/com/example/CourseHive/CourseHiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.CourseHive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseHiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseHiveApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/example/CourseHive/CourseHiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CourseHive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseHiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
